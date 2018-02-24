7a109d0fc6b50f741c7bd8d6f29dc2257aee5302
zhangpeng <zhangpeng25@58ganji.com>
2018-02-24 17:57:47 +0800

    koa版本

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-const express = require('express')
+const koa = require('koa')
+const Router = require('koa-router')
 const next = require('next')
 
+const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare()
     .then(() => {
-        const server = express()
+        const server = new koa()
+        const router = new Router()
 
-        server.get('/p/:id', (req, res) => {
-            const actualPage = '/post'
-            const queryParams = { id: req.params.id }
-            app.render(req, res, actualPage, queryParams)
+        router.get('*', async ctx => {
+            await handle(ctx.req, ctx.res)
+            ctx.respond = false
         })
 
-        server.get('*', (req, res) => {
-            return handle(req, res)
+        server.use(async (ctx, next) => {
+            ctx.res.statusCode = 200
+            await next()
         })
 
-        server.listen(3000, (err) => {
+        server.use(router.routes())
+        server.listen(port, (err) => {
             if (err) throw err
-            console.log('> Ready on http://localhost:3000')
+            console.log(`> Ready on http://localhost:${port}`)
         })
     })
     .catch((ex) => {
