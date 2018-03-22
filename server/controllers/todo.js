0df41ee01bf2836a64165ccd2d644ccc30246425
zhangpeng <zhangpeng25@58ganji.com>
2018-03-22 18:01:16 +0800

    增加参数查询

--- server/controllers/todo.js
+++ server/controllers/todo.js
@@ -3,24 +3,28 @@ const TodoModel = require('../models/Todo');
 const todoModel = new TodoModel();
 
 exports.addTodo = async (ctx, next) => {
+    console.log(ctx.query);
     await todoModel.saveTodo({todoContent: 'hahahha'}).then(()=>{
         console.log('增加成功');
     });
 }
 
 exports.delTodo = async (ctx, next) => {
+    console.log(ctx.query);
     await todoModel.delTodo({todoContent: 'hahahha'}).then(()=>{
         console.log('删除成功');
     });
 }
 
 exports.changeTodo = async (ctx, next) => {
+    console.log(ctx.query);
     await todoModel.changeTodo({}).then(()=>{
         console.log('修改成功');
     });
 }
 
 exports.getTodo = async (ctx, next) => {
+    console.log(ctx.query);
     await todoModel.getTodo({}).then((doc)=>{
         console.log('查询数据');
         utils.response(doc,ctx)
